53bdc642c59109cf616ac9b2e1b0af9452969464
Barabas Raffai <barabas.raffai@gmail.com>
2025-01-11 15:02:19 +0000

    WIP

---
+++ trunk/config.h
@@ -0,0 +1,50 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_CONFIG_H_WAF
+#define W_CONFIG_H_WAF
+
+#define IS_LINUX "linux"
+#define LOCALEDIR "/home/barabas/tmp/guitarix-prefix/share/locale"
+#define GETTEXT_PACKAGE "guitarix"
+#define ENABLE_NLS 1
+#define HAVE_SNDFILE 1
+#define HAVE_FFTW3_H 1
+#define HAVE_FFTW3 1
+#define HAVE_JACK 1
+#define HAVE_GMODULE_EXPORT 1
+#define HAVE_CURL 1
+#define HAVE_GTHREAD 1
+#define HAVE_GLIBMM 1
+#define HAVE_GTK2 1
+#define HAVE_GTKMM 1
+#define HAVE_GIOMM 1
+/* #undef HAVE_LADSPA_H */
+#define HAVE_LRDF 1
+#define HAVE_LILV 1
+#define ZITA_CONVOLVER_VERSION 3
+#define GX_STYLE_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/skins"
+#define GX_FACTORY_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/factorysettings"
+#define GX_SOUND_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/sounds"
+#define GX_SOUND_BPB_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/sounds/bands"
+#define GX_SOUND_BPA_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/sounds/amps"
+#define GX_BUILDER_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/builder"
+#define GX_ICON_DIR "/home/barabas/tmp/guitarix-prefix/share/guitarix/icons"
+#define GX_PIXMAPS_DIR "/home/barabas/tmp/guitarix-prefix/share/pixmaps"
+#define GX_METAINFO_DIR "/home/barabas/tmp/guitarix-prefix/share/metainfo"
+#define GX_VERSION "0.46.1-git"
+#define OS_64_BIT 1
+#define HAVE_SIGC 1
+#define HAVE_LV2CORE 1
+#define GX_LV2_STYLE_DIR "/home/barabas/tmp/guitarix-prefix/share/gx_head/skins/LV2"
+#define NOOPT 1
+#define HAVE_AVAHI_GOBJECT 1
+#define HAVE_AVAHI_GLIB 1
+#define HAVE_AVAHI_CLIENT 1
+#define HAVE_AVAHI 1
+#define HAVE_BLUEZ 1
+#define HAVE_LIBLO 1
+#define HAVE_EIGEN3 1
+#define HAVE_CAIRO 1
+#define HAVE_X11_XLIB_H 1
+
+#endif /* W_CONFIG_H_WAF */
